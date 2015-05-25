64238e5ad3902943c8e9cab33a277dcb9bdee24a
nkopa <natalia16k@wp.pl>
2015-05-25 22:11:51 +0000

    poprawka migracji2

--- db/migrate/20150525205826_drop_review_table.rb
+++ db/migrate/20150525205826_drop_review_table.rb
@@ -1,6 +1,6 @@
 class DropReviewTable < ActiveRecord::Migration
   def change
-    remove_foreign_key :reviews, name: :fk_rails_5350d1b47c
+    remove_foreign_key :comments, name: :fk_rails_5350d1b47c
     drop_table :reviews
   end
 end
